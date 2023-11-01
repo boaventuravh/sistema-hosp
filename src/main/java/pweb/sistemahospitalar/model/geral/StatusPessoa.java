7d3dde8ecc8116a741a468f6faca4beab5acabea
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2023-11-01 10:11:53 -0300

    criada classe StatusPessoa

---
+++ src/main/java/pweb/sistemahospitalar/model/geral/StatusPessoa.java
@@ -0,0 +1,25 @@
+package pweb.sistemahospitalar.model.geral;
+
+import java.util.UUID;
+
+public class StatusPessoa {
+
+    private UUID id;
+    private String descricao;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
