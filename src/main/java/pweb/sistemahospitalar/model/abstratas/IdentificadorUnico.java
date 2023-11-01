bd98ae248cd59e302f2674d0b7fd4b44197ad829
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2023-11-01 10:24:39 -0300

    add id, remoção de construtor e add getters e setters

--- src/main/java/pweb/sistemahospitalar/model/abstratas/IdentificadorUnico.java
+++ src/main/java/pweb/sistemahospitalar/model/abstratas/IdentificadorUnico.java
@@ -1,11 +1,16 @@
 package pweb.sistemahospitalar.model.abstratas;
 
+import java.util.UUID;
+
 public abstract class IdentificadorUnico {
+
+    protected UUID id;
     protected String digitos;
 
-    public IdentificadorUnico(String digitos){
-        this.digitos = digitos;
-    }
+
+    public abstract UUID getId();
+    public abstract void setId(UUID id);
     public abstract String getDigitos();
+    public abstract void setDigitos(String digitos);
 
 }
