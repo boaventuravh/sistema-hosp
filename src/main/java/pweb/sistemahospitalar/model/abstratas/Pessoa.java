ebbf140239a152a4268e7d81c8b93d5101c4a655
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2023-11-01 10:19:28 -0300

    acrescentado id e statusId + alterado endereco para enderecoId

--- src/main/java/pweb/sistemahospitalar/model/abstratas/Pessoa.java
+++ src/main/java/pweb/sistemahospitalar/model/abstratas/Pessoa.java
@@ -1,27 +1,28 @@
 package pweb.sistemahospitalar.model.abstratas;
 
 import pweb.sistemahospitalar.model.geral.Endereco;
+import pweb.sistemahospitalar.model.geral.StatusPessoa;
+
+import java.util.UUID;
 
 public abstract class Pessoa {
 
+    protected UUID id;
     protected String nome;
     protected String email;
     protected String telefone;
-    protected Endereco endereco;
+    protected UUID enderecoId;
+    protected UUID statusId;
 
-    public Pessoa(){}
-
-    public Pessoa(String nome, String email, String telefone, Endereco endereco) {
-        this.nome = nome;
-        this.email = email;
-        this.telefone = telefone;
-        this.endereco = endereco;
-    }
+    public abstract UUID getId();
+    public abstract void setId(UUID id);
     public abstract String getNome();
     public abstract void setEmail(String email);
     public abstract String getEmail();
     public abstract void setTelefone(String telefone);
     public abstract String getTelefone();
-    public abstract void setEndereco(Endereco endereco);
-    public abstract Endereco getEndereco();
+    public abstract void setEnderecoId(UUID enderecoId);
+    public abstract UUID getEnderecoId();
+    public abstract UUID getStatusId();
+    public abstract void setStatusId(UUID statusId);
 }
