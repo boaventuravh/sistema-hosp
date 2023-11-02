ec17393a485b4501530e0c8526e56c5873567b1c
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2023-11-02 14:29:23 -0300

    adicionado atributo id + getters e setters

--- src/main/java/pweb/sistemahospitalar/model/medico/Especialidade.java
+++ src/main/java/pweb/sistemahospitalar/model/medico/Especialidade.java
@@ -1,14 +1,26 @@
 package pweb.sistemahospitalar.model.medico;
+
+import java.util.UUID;
+
 //Ortopedia, Cardiologia, Ginecologia ou Dermatologia
 public class Especialidade {
 
+    private UUID id;
     private String nomeEspecialidade;
-
-    Especialidade(String nomeEspecialidade) {
-        this.nomeEspecialidade = nomeEspecialidade;
-    }
 
     public String getNomeEspecialidade() {
         return nomeEspecialidade;
+    }
+
+    public void setNomeEspecialidade(String nomeEspecialidade) {
+        this.nomeEspecialidade = nomeEspecialidade;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
     }
 }
