8c9153bad20bff50c22247a56d61eeb3d2bcb105
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2023-11-02 14:28:06 -0300

    adicionado atributo id + getters e setters

--- src/main/java/pweb/sistemahospitalar/model/consulta/Consulta.java
+++ src/main/java/pweb/sistemahospitalar/model/consulta/Consulta.java
@@ -5,10 +5,43 @@ import pweb.sistemahospitalar.model.paciente.Paciente;
 
 import java.util.Date;
 import java.util.TimeZone;
+import java.util.UUID;
 
 public class Consulta {
-    private Medico medico;
-    private Paciente paciente;
+    private UUID id;
+    private UUID medicoId;
+    private UUID pacienteId;
     private Date dataHora;
 
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getMedicoId() {
+        return medicoId;
+    }
+
+    public void setMedicoId(UUID medicoId) {
+        this.medicoId = medicoId;
+    }
+
+    public UUID getPacienteId() {
+        return pacienteId;
+    }
+
+    public void setPacienteId(UUID pacienteId) {
+        this.pacienteId = pacienteId;
+    }
+
+    public Date getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(Date dataHora) {
+        this.dataHora = dataHora;
+    }
 }
