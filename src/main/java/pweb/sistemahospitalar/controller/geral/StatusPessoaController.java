efaade5487d428870217cc6e7c001c57b0f8b04f
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2023-11-07 04:55:05 -0300

    criação do método Post

--- src/main/java/pweb/sistemahospitalar/controller/geral/StatusPessoaController.java
+++ src/main/java/pweb/sistemahospitalar/controller/geral/StatusPessoaController.java
@@ -1,2 +1,26 @@
-package pweb.sistemahospitalar.controller.geral;public class StatusPessoaController {
+package pweb.sistemahospitalar.controller.geral;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import pweb.sistemahospitalar.dtos.geral.StatusPessoaRecordDto;
+import pweb.sistemahospitalar.model.geral.StatusPessoaModel;
+import pweb.sistemahospitalar.repositories.geral.StatusPessoaRepository;
+
+@RestController
+public class StatusPessoaController {
+    @Autowired
+    StatusPessoaRepository statusPessoaRepository;
+
+    @PostMapping("/statuspessoa")
+    public ResponseEntity<StatusPessoaModel> saveStatusPessoa(@RequestBody @Valid StatusPessoaRecordDto statusPessoaRecordDto){
+        var statusPessoaModel = new StatusPessoaModel();
+        BeanUtils.copyProperties(statusPessoaRecordDto, statusPessoaModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(statusPessoaRepository.save(statusPessoaModel));
+    }
 }
