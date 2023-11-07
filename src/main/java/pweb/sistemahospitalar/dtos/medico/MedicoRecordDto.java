53f3ecf64bdef6e33c46cb5d06303909c2fdf528
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2023-11-07 15:37:52 -0300

    alteração de nomenclatura

--- src/main/java/pweb/sistemahospitalar/dtos/medico/MedicoRecordDto.java
+++ src/main/java/pweb/sistemahospitalar/dtos/medico/MedicoRecordDto.java
@@ -5,5 +5,5 @@ import pweb.sistemahospitalar.model.geral.EnderecoModel;
 import pweb.sistemahospitalar.model.geral.StatusPessoaModel;
 import pweb.sistemahospitalar.model.medico.EspecialidadeModel;
 
-public record MedicoRecordDto(@NotNull String nome, @NotNull String email, @NotNull String telefone, @NotNull StatusPessoaModel status, @NotNull EnderecoModel enderecoModel, @NotNull String crm, @NotNull EspecialidadeModel especialidadeModel) {
+public record MedicoRecordDto(@NotNull String nome, @NotNull String email, @NotNull String telefone, @NotNull StatusPessoaModel status, @NotNull EnderecoModel endereco, @NotNull String crm, @NotNull EspecialidadeModel especialidade) {
 }
--- src/main/java/pweb/sistemahospitalar/dtos/paciente/PacienteRecordDto.java
+++ src/main/java/pweb/sistemahospitalar/dtos/paciente/PacienteRecordDto.java
@@ -4,5 +4,5 @@ import jakarta.validation.constraints.NotNull;
 import pweb.sistemahospitalar.model.geral.EnderecoModel;
 import pweb.sistemahospitalar.model.geral.StatusPessoaModel;
 
-public record PacienteRecordDto(@NotNull String nome, @NotNull String email, @NotNull String telefone, @NotNull StatusPessoaModel status, @NotNull EnderecoModel enderecoModel, @NotNull String cpf) {
+public record PacienteRecordDto(@NotNull String nome, @NotNull String email, @NotNull String telefone, @NotNull StatusPessoaModel status, @NotNull EnderecoModel endereco, @NotNull String cpf) {
 }
