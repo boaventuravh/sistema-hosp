5d36eec73cbed11ce6b6be62d6b8e0e701677955
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2023-11-08 16:45:25 -0300

    criação do método getAll

--- src/main/java/pweb/sistemahospitalar/controller/medico/MedicoController.java
+++ src/main/java/pweb/sistemahospitalar/controller/medico/MedicoController.java
@@ -6,16 +6,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import pweb.sistemahospitalar.dtos.completo.medico.MedicoRecordDto;
+import pweb.sistemahospitalar.dtos.listagem.MedicoListRecordDto;
 import pweb.sistemahospitalar.model.geral.EnderecoModel;
 import pweb.sistemahospitalar.model.medico.MedicoModel;
 import pweb.sistemahospitalar.repositories.geral.EnderecoRepository;
 import pweb.sistemahospitalar.repositories.geral.StatusPessoaRepository;
 import pweb.sistemahospitalar.repositories.medico.EspecialiadadeRepository;
 import pweb.sistemahospitalar.repositories.medico.MedicoRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class MedicoController {
@@ -49,6 +54,15 @@ public class MedicoController {
         catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
+    }
+
+    @GetMapping("/medico")
+    public ResponseEntity<List<MedicoModel>> listarMedicos(){
+        var list = medicoRepository.findAll();
+
+        list.removeIf(m -> m.getStatus().getDescricao().equalsIgnoreCase("inativo"));
+
+        return ResponseEntity.status(HttpStatus.OK).body(list);
     }
 
 }
