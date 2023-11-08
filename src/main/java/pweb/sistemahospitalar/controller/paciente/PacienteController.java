c7577223db5d591562f39fd57970d66a0d8484a2
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2023-11-08 15:39:59 -0300

    criação de PacienteController com método POST

---
+++ src/main/java/pweb/sistemahospitalar/controller/paciente/PacienteController.java
@@ -0,0 +1,50 @@
+package pweb.sistemahospitalar.controller.paciente;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import pweb.sistemahospitalar.dtos.paciente.PacienteRecordDto;
+import pweb.sistemahospitalar.model.geral.EnderecoModel;
+import pweb.sistemahospitalar.model.paciente.PacienteModel;
+import pweb.sistemahospitalar.repositories.geral.EnderecoRepository;
+import pweb.sistemahospitalar.repositories.geral.StatusPessoaRepository;
+import pweb.sistemahospitalar.repositories.paciente.PacienteRepository;
+
+@RestController
+public class PacienteController {
+
+    @Autowired
+    PacienteRepository pacienteRepository;
+    @Autowired
+    EnderecoRepository enderecoRepository;
+    @Autowired
+    StatusPessoaRepository statusRepository;
+
+    @PostMapping("/paciente")
+    @Transactional
+    public ResponseEntity<PacienteModel> savePaciente(@RequestBody @Valid PacienteRecordDto pacienteRecordDto){
+        var pacienteModel = new PacienteModel();
+        BeanUtils.copyProperties(pacienteRecordDto, pacienteModel);
+
+        var enderecoModel = new EnderecoModel();
+        BeanUtils.copyProperties(pacienteModel.getEndereco(), enderecoModel);
+
+        enderecoRepository.save(enderecoModel);
+
+        pacienteModel.setEndereco(enderecoRepository.getReferenceById(enderecoModel.getId()));
+        pacienteModel.setStatus(statusRepository.findByDescricao(pacienteRecordDto.status().getDescricao()));
+
+        try {
+            return ResponseEntity.status(HttpStatus.CREATED).body(pacienteRepository.save(pacienteModel));
+        }
+        catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+}
