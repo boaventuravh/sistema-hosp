1859eb9d8cdb7629bb0bf132425937891cc9689e
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2023-11-09 20:13:54 -0300

    alteração em Especialidade para armazenar apenas a descricao

--- src/main/java/pweb/sistemahospitalar/dtos/listagem/MedicoListRecordDto.java
+++ src/main/java/pweb/sistemahospitalar/dtos/listagem/MedicoListRecordDto.java
@@ -3,5 +3,5 @@ package pweb.sistemahospitalar.dtos.listagem;
 import jakarta.validation.constraints.NotNull;
 import pweb.sistemahospitalar.model.medico.EspecialidadeModel;
 
-public record MedicoListRecordDto(@NotNull String nome, @NotNull String email, @NotNull String crm, @NotNull EspecialidadeModel especialidade) {
+public record MedicoListRecordDto(@NotNull String nome, @NotNull String email, @NotNull String crm, @NotNull String especialidade) {
 }
