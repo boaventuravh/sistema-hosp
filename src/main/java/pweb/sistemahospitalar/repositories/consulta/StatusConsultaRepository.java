0252ee5a2b10ffe632b5b2010daa5a74b700413e
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2023-11-14 17:05:53 -0300

    add método findByDescricao

--- src/main/java/pweb/sistemahospitalar/repositories/consulta/StatusConsultaRepository.java
+++ src/main/java/pweb/sistemahospitalar/repositories/consulta/StatusConsultaRepository.java
@@ -8,4 +8,5 @@ import java.util.UUID;
 
 @Repository
 public interface StatusConsultaRepository extends JpaRepository<StatusConsultaModel, UUID> {
+    StatusConsultaModel findByDescricao(String descricao);
 }
