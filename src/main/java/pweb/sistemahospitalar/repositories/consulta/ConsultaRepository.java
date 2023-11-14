04a98644bcaee2c761c09eadcf4ec62b4ed7f456
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2023-11-14 17:07:16 -0300

    add método findById

--- src/main/java/pweb/sistemahospitalar/repositories/consulta/ConsultaRepository.java
+++ src/main/java/pweb/sistemahospitalar/repositories/consulta/ConsultaRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pweb.sistemahospitalar.model.consulta.ConsultaModel;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ConsultaRepository extends JpaRepository<ConsultaModel, UUID> {
+    Optional<ConsultaModel> findById(UUID id);
 }
