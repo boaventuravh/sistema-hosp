40d546dfa7cada2075aa3a07d8ae34cc97561a3c
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2023-11-14 15:30:26 -0300

    adequação à alteração feita na model Consulta

--- src/main/java/pweb/sistemahospitalar/dtos/completo/consulta/ConsultaRecordDto.java
+++ src/main/java/pweb/sistemahospitalar/dtos/completo/consulta/ConsultaRecordDto.java
@@ -1,9 +1,10 @@
 package pweb.sistemahospitalar.dtos.completo.consulta;
 
 import jakarta.validation.constraints.NotNull;
-import pweb.sistemahospitalar.model.paciente.PacienteModel;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
-public record ConsultaRecordDto(@NotNull String crmMedico, @NotNull String cpfPaciente, @NotNull Date dataHora) {
+public record ConsultaRecordDto(@NotNull String crmMedico, @NotNull String cpfPaciente, @NotNull LocalDate data, @NotNull
+                                LocalTime horario) {
 }
